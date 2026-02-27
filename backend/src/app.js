433810dbeb630e3a608984d2dd43d4f9c8a93582
Goverdhan <govardhandevari@gmail.com>
2026-02-27 12:30:29 +0530

    sales person target page changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,19 +11,12 @@ app.use(express.json());
 const health = require('./modules/health/health.routes');
 const deptRevenueRoutes = require("./modules/deptRevenue/deptRevenue.routes");
 const PaymentModeRoutes = require('./modules/payments/payments.routes');
+const salespersonTargetRoutes = require('./modules/salespersonTargets/salespersonTargets.routes');
 
-// Routes
-
-//app.use('/api/states', require('./modules/state/state.routes'));
-//app.use('/api/cities', require('./modules/city/city.routes'));
-//app.use('/api/centres', require('./modules/centre/centre.routes'));
 app.use('/api/health', health);
 app.use('/api/payments', PaymentModeRoutes);
 app.use("/api/dept-revenue", deptRevenueRoutes);
-
-
-
-
+app.use('/api/salesperson-targets', salespersonTargetRoutes);
 
 // Error handler (must be last)
 app.use(errorHandler);
